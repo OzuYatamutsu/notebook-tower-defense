d01afcf45cfe0e26ee1bf6d760af0d438feca0d3
Sean Collins <seanc@seancotech.com>
2025-03-11 07:44:25 +0700

    Start shooting arrows from arrow towers

--- towers/arrow_tower.gd
+++ towers/arrow_tower.gd
@@ -1,14 +1,26 @@
 class_name ArrowTower
 extends StaticBody2D
 
-@export var PROJECTILE = ""
+const PROJECTILE_REF: PackedScene = preload("res://projectiles/Arrow.tscn")
+
 @export var CURRENT_TARGET: Basic
-@export var RATE_OF_FIRE = 1.0
+@export var RATE_OF_FIRE_SECS = 1.0
+
+@onready var ShootTimer: Timer = $Turret/ShootTimer
+@onready var ProjectileRoot: Node2D = get_tree().get_first_node_in_group("projectiles")
 
 func _ready() -> void:
-    # TODO
-    # assert(PROJECTILE != null, "Towers need a projectile assigned!")
-    pass
+    assert(
+        PROJECTILE_REF != null,
+        "Towers need a projectile assigned!"
+    )
+    assert(
+        get_tree().has_group("projectiles"),
+        "Towers need a projectile base node in the scene " +
+        "tree to spawn projectiles!"
+    )
+    ShootTimer.wait_time = RATE_OF_FIRE_SECS
+    ShootTimer.start()
 
 func _process(delta: float) -> void:
     pass
@@ -21,3 +33,13 @@ func _on_targeting_radius_entered(body: Basic) -> void:
 func _on_targeting_radius_exited(body: Node2D) -> void:
     print(str(self) + ": Lost my target, " + str(body))
     CURRENT_TARGET = null
+
+func _on_shoot_timer_timeout() -> void:
+    if CURRENT_TARGET == null:
+        return
+
+    var new_projectile: Projectile = PROJECTILE_REF.instantiate()
+    ProjectileRoot.add_child(new_projectile)
+    new_projectile.init(CURRENT_TARGET.global_position)
+
+    print(str(self) + ": Shooting an arrow, " + str(new_projectile))
