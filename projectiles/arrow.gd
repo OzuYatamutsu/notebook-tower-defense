220270e39f5b8443483251adcc6228839b5e6442
Sean Collins <seanc@seancotech.com>
2025-03-11 19:46:43 +0700

    Rotate the head of arrow towards the mob

--- projectiles/arrow.gd
+++ projectiles/arrow.gd
@@ -3,3 +3,11 @@ extends Projectile
 
 func _ready() -> void:
     self.SPEED = 100.0
+
+func _physics_process(delta: float) -> void:
+    if !IS_ACTIVE:
+        return
+    
+    # We want to point the head of the arrow at
+    # what we're firing
+    rotation = position.angle_to_point(TARGET_LOCATION)
