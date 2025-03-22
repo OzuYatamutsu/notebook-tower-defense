a84d0b1adc2e76059ae7c63bbb5c70344abc9998
Sean Collins <seanc@seancotech.com>
2025-03-22 18:17:41 +0800

    Start hooking up mobs to waves

--- levels/level.gd
+++ levels/level.gd
@@ -1,6 +1,11 @@
 class_name Level
 extends Node
 
+var WAVE_CONTENTS: Array[Dictionary]
+var WAVES: Array[Wave] = []
+
 func _ready() -> void:
     GameState.PLAYER_LIVES_START = 5  # TODO
+    for wave_contents in WAVE_CONTENTS:
+        WAVES.append(Wave.new(wave_contents))
     GameState._on_level_load()
--- levels/test_level.gd
+++ levels/test_level.gd
@@ -1,1 +1,6 @@
 extends Level
+
+func _init() -> void:
+    WAVE_CONTENTS = [
+        {Basic: 5}
+    ]
