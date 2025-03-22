cec082560582ea95e87ce0b39f617c145a314a56
Sean Collins <seanc@seancotech.com>
2025-03-22 19:11:44 +0800

    Enforce required nodes are ready at level load

--- components/game_state.gd
+++ components/game_state.gd
@@ -1,6 +1,9 @@
 extends Node
 
 const LEVEL_HITBOX_GROUP = "level_hitbox"
+const MOBS_GROUP = "mobs"
+const TOWERS_GROUP = "towers"
+const PROJECTILES_GROUP = "projectiles"
 
 @export var PLAYER_LIVES_START: int
 @export var PLAYER_LIVES_REMAINING: int
@@ -11,7 +14,11 @@ func _on_level_load() -> void:
 
     var hitbox: LevelHitbox = get_tree().get_first_node_in_group(LEVEL_HITBOX_GROUP)
     hitbox.connect(hitbox.mob_slipped_through.get_name(), _on_mob_slipped_through)
+
     assert(PLAYER_LIVES_REMAINING > 0, "Lives left on level start should be > 0!")
+    assert(get_tree().get_first_node_in_group(GameState.MOBS_GROUP), "Missing a mobs node!")
+    assert(get_tree().get_first_node_in_group(GameState.TOWERS_GROUP), "Missing a towers node!")
+    assert(get_tree().get_first_node_in_group(GameState.PROJECTILES_GROUP), "Missing a projectiles node!")
 
 func _on_mob_slipped_through() -> void:
     PLAYER_LIVES_REMAINING -= 1
