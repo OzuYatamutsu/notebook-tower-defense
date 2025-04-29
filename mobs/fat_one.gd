c12c9e65cc1a24357b2566b2bd6ade1952cc6420
Sean Collins <seanc@seancotech.com>
2025-04-29 23:11:42 +0800

    Add nyoom, fatone mob types

---
+++ mobs/fat_one.gd
@@ -0,0 +1,7 @@
+class_name FatOne
+extends Mob
+
+func _init():
+    self.MAX_HP = 250
+    self.SPEED = 0.02
+    self.VALUE = 250
---
+++ mobs/nyoom.gd
@@ -0,0 +1,7 @@
+class_name Nyoom
+extends Mob
+
+func _init():
+    self.MAX_HP = 20
+    self.SPEED = 0.25
+    self.VALUE = 10
