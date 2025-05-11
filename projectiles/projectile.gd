aa66543a2108cb7f4585936c143f236a51d62213
Sean Collins <seanc@seancotech.com>
2025-05-11 18:50:09 +0800

    Fix 'in a wall' calculation for projectiles

--- projectiles/projectile.gd
+++ projectiles/projectile.gd
@@ -2,13 +2,14 @@ class_name Projectile
 extends Area2D
 
 const DESPAWN_TIMER_SECS = 10.0
-
+const INITIAL_IGNORE_WALLS_RECALC_GRACE_PERIOD_SECS = 0.1
 @export var SPEED: float
 @export var TARGET: Mob
 @export var IS_ACTIVE = false
 @export var DAMAGE: float
 var EFFECTS: Array[Effect]
 var AVERAGE_DELTA: float
+var IgnoreWalls: bool = true
 
 @onready var DespawnTimer = $DespawnTimer
 
@@ -16,12 +17,12 @@ var direction: Vector2 = Vector2.ZERO
 
 func _init():
     self.collision_layer = 0x8  # projectiles
-    self.collision_mask = 0x2  # walls
+    self.collision_mask = 0x3  # walls, towers
     area_entered.connect(_on_hit_wall)
 
 func fire(at_direction: Vector2) -> void:
     assert(SPEED != 0.0, "Projectiles must have a speed set!")
-    
+
     DespawnTimer.wait_time = DESPAWN_TIMER_SECS
     DespawnTimer.start()
     IS_ACTIVE = true
@@ -80,11 +81,27 @@ func _process(delta: float) -> void:
         return
 
     global_position += direction * SPEED * delta
+    if IgnoreWalls:
+        await get_tree().create_timer(INITIAL_IGNORE_WALLS_RECALC_GRACE_PERIOD_SECS).timeout
+        _recalculate_should_ignore_walls()
+
+func _recalculate_should_ignore_walls() -> void:
+    for area in get_overlapping_areas():
+        if area is Tower:
+            return
+    IgnoreWalls = false
+    _recalc_in_a_wall_despawn_if_necessary()
+
+func _recalc_in_a_wall_despawn_if_necessary() -> void:
+    if !get_overlapping_areas().is_empty():
+        queue_free()
 
 func _on_despawn_timer_timeout() -> void:
     # We didn't hit the target in time
     # so clean this up
     queue_free()
 
-func _on_hit_wall(_wall: Area2D) -> void:
+func _on_hit_wall(_wall_or_tower: Area2D) -> void:
+    if IgnoreWalls:
+        return
     queue_free()
