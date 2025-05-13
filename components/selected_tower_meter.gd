73d171980256d7e6dc8ea058fe4a453e6a57816d
Sean Collins <seanc@seancotech.com>
2025-05-13 16:27:27 +0800

    Fix selected tower meter erroroneously showing a tower not selected on start

--- components/selected_tower_meter.gd
+++ components/selected_tower_meter.gd
@@ -3,6 +3,9 @@ extends Control
 
 @onready var SelectedTowerSprite = $Sprite
 
+func _ready() -> void:
+    SelectedTowerSprite.texture = null
+
 func update_sprite() -> void:
     SelectedTowerSprite.texture = \
         GameState.TOWER_PLACEMENT_SHADOW.TOWER_TO_PLACE\
