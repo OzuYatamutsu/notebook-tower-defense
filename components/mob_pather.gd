b447e6dbdc4b39e424d7447cf874ec998bc817bf
Sean Collins <seanc@seancotech.com>
2025-05-17 11:31:55 +0700

    Enable fast mobs

--- components/mob_pather.gd
+++ components/mob_pather.gd
@@ -11,10 +11,14 @@ func _ready() -> void:
     child = get_child(0)
 
 func _physics_process(delta: float) -> void:
-    if progress_ratio >= 100.0:
+    if (
+        progress_ratio >= 100.0
+        or progress_ratio + (child.SPEED * delta) >= 100.0
+    ):
         progress_ratio = 100.0
         return
-    progress_ratio += child.SPEED * delta
+    progress_ratio += (child.SPEED * delta)
+    
 
 func _on_mob_despawn(_mob) -> void:
     queue_free()
--- levels/test_level.gd
+++ levels/test_level.gd
@@ -2,9 +2,10 @@ extends Level
 
 func _init() -> void:
     WAVE_CONTENTS = [
-        {"res://mobs/Basic.tscn": 5, "res://mobs/FatOne.tscn": 1},
-        {"res://mobs/Basic.tscn": 10, "res://mobs/FatOne.tscn": 5},
-        {"res://mobs/Basic.tscn": 30, "res://mobs/FatOne.tscn": 10}
+        {"res://mobs/Nyoom.tscn": 5}
+        #{"res://mobs/Basic.tscn": 5, "res://mobs/FatOne.tscn": 1},
+        #{"res://mobs/Basic.tscn": 10, "res://mobs/FatOne.tscn": 5},
+        #{"res://mobs/Basic.tscn": 30, "res://mobs/FatOne.tscn": 10}
     ]
 
 func _ready():
