40d92345af725b9cf1c16e97c824987150d74aeb
Sean Collins <seanc@seancotech.com>
2025-05-22 13:30:33 +0800

    Implement UI for upgrade tower panel

---
+++ components/tower_upgrade_item.gd
@@ -0,0 +1,56 @@
+class_name TowerUpgradeItem
+extends Control
+
+const CLEAR_DIGIT = 10
+
+@export var Cost: int
+@export var TowerPath: String
+
+@onready var TowerSprite: Sprite2D = $TowerSprite
+@onready var UpgradeEffects: BoxContainer = $UpgradeEffects
+
+@onready var digit1 = $NumericLabel/digit1
+@onready var digit2 = $NumericLabel/digit2
+@onready var digit3 = $NumericLabel/digit3
+@onready var digit4 = $NumericLabel/digit4
+
+func setup(towerPath: String) -> void:
+    var tower = load(towerPath).instantiate()
+
+    Cost = tower.VALUE
+    TowerPath = towerPath
+
+    get_node("TowerSprite").texture = tower.get_node("Sprite").texture
+    tower.queue_free()
+    set_anchors_preset(Control.PRESET_FULL_RECT)
+
+func _ready() -> void:
+    _set_numeric_label()
+
+func _set_numeric_label() -> void:
+    digit1.frame = CLEAR_DIGIT
+    digit2.frame = CLEAR_DIGIT
+    digit3.frame = CLEAR_DIGIT
+    digit4.frame = CLEAR_DIGIT
+    
+    if Cost <= 0:
+        Cost = 0
+    elif Cost >= 9999:
+        Cost = 9999
+
+    var str_value = str(Cost)
+    var length = str_value.length()
+
+    # Alignment
+    var start_index = 4 - length
+
+    for i in range(length):
+        var digit = int(str_value[i])
+        match start_index + i:
+            0: digit1.frame = digit
+            1: digit2.frame = digit
+            2: digit3.frame = digit
+            3: digit4.frame = digit
+
+func _on_button_pressed() -> void:
+    pass  # TODO
--- components/tower_upgrade_panel.gd
+++ components/tower_upgrade_panel.gd
@@ -1,2 +1,22 @@
 class_name TowerUpgradePanel
 extends Control
+
+@export var SelectedTower: Tower
+
+@onready var CurrentTowerSprite: Sprite2D = $CurrentTowerSprite
+@onready var UpgradedComponent: Control = $UpgradedComponent
+@onready var ToUpgradeComponent: Control = $ToUpgradeComponent
+@onready var UpgradeItemList: VBoxContainer = $ToUpgradeComponent/UpgradeItemList
+
+var CurrentTowerIsUpgraded: bool = false
+
+func _ready() -> void:
+    for child in UpgradeItemList.get_children():
+        UpgradeItemList.remove_child(child)
+
+func set_current_tower(tower: Tower):
+    SelectedTower = tower
+    CurrentTowerSprite.texture = SelectedTower.Sprite.texture
+
+func _on_close_button_pressed() -> void:
+    hide()
