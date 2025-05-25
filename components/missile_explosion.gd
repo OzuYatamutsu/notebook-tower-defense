f6594b4a882474c873da76c3e02aadb7f809e0ae
Sean Collins <seanc@seancotech.com>
2025-05-25 16:47:27 +0800

    Add explosion effect

---
+++ components/missile_explosion.gd
@@ -0,0 +1,27 @@
+class_name MissileExplosion
+extends Area2D
+
+const EXPLOSION_GROW_SPEED: float = 175.0
+const MAX_EXPLOSION_RADIUS: float = 100.0
+const EXPLOSION_COLOR: Color = Color(1.0, 0.4, 0.4)
+const DAMAGE: float = 50.0
+
+var radius: float = 1.0
+
+@onready var ExplosionCollider: CollisionShape2D = $ExplosionCollider
+
+func _process(delta) -> void:
+    radius += EXPLOSION_GROW_SPEED * delta
+    if radius > MAX_EXPLOSION_RADIUS:
+        _on_max_explosion_radius()
+    queue_redraw()
+
+func _on_max_explosion_radius() -> void:
+    queue_free()
+
+func _draw() -> void:
+    draw_circle(position, radius, EXPLOSION_COLOR)
+    ExplosionCollider.shape.radius = radius
+
+func _on_area_entered(mob: Mob) -> void:
+    mob.HEALTH_BAR.damage_by(DAMAGE)
--- projectiles/missile.gd
+++ projectiles/missile.gd
@@ -1,2 +1,25 @@
 class_name Missile
 extends Projectile
+
+const EXPLOSION_TIMER_SECS: float = 3.0
+
+@onready var ExplosionTimer: Timer = $ExplosionTimer
+
+func _ready() -> void:
+    self.SPEED = 100.0
+    self.DAMAGE = 50.0
+    self.EFFECTS.append(Effect.new(self.effect))
+    ExplosionTimer.wait_time = EXPLOSION_TIMER_SECS
+
+func fire_at(mob: Mob) -> void:
+    super.fire_at(mob)
+    ExplosionTimer.start()
+
+func _explode() -> void:
+    pass
+
+func effect(_mob: Mob) -> void:
+    _explode()
+
+func _on_explosion_timer_timeout() -> void:
+    _explode()
