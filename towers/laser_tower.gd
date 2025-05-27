2d8b4aae4475377deeea9721f7c0f6a7be5828e0
Sean Collins <seanc@seancotech.com>
2025-05-27 11:17:07 +0800

    Increase laser tower base rate of fire

--- towers/laser_tower.gd
+++ towers/laser_tower.gd
@@ -12,7 +12,7 @@ func _ready() -> void:
     ready_tower(
         preload("res://projectiles/Laser.tscn"),
         1000.00,  # Range
-        3.5  # Rate of fire
+        2.5  # Rate of fire
     )
 
     enable()
