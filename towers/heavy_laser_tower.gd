c1db3330eb4b5a1dc7ffab0869e0d0f9266a4a96
Sean Collins <seanc@seancotech.com>
2025-05-28 12:57:25 +0800

    Implement heavy laser tower

--- towers/heavy_laser_tower.gd
+++ towers/heavy_laser_tower.gd
@@ -1,6 +1,12 @@
 class_name HeavyLaserTower
 extends Tower
 
+const RETARGETING_PERIOD_SECS = 1.0
+
+@onready var RetargetingTimer = $RetargetingTimer
+
+var IS_ACTIVELY_FIRING: bool = false
+
 func _init() -> void:
     VALUE = 500
     IsUpgraded = true
@@ -14,9 +20,38 @@ func _init() -> void:
 
 func _ready() -> void:
     ready_tower(
-        preload("res://projectiles/Laser.tscn"),
-        1000.00,  # Range
-        1.5  # Rate of fire
+        preload("res://projectiles/HeavyLaser.tscn"),
+        1500.00,  # Range
+        3.0  # Rate of fire
     )
-
+    
+    RetargetingTimer.wait_time = RETARGETING_PERIOD_SECS
+    RetargetingTimer.start()
     enable()
+
+func retarget() -> void:
+    if TargetingQueue.is_empty():
+        return
+
+    TargetingQueue.append(CURRENT_TARGET)
+    CURRENT_TARGET = null
+    
+    TargetingQueue.sort_custom(_sort_mobs_by_hp)
+    CURRENT_TARGET = TargetingQueue.pop_front()
+    ShouldFireImmediately = true
+
+func fire() -> void:
+    if IS_ACTIVELY_FIRING:
+        return
+    super.fire()
+    IS_ACTIVELY_FIRING = true
+
+func _on_targeting_radius_exited(body: Mob) -> void:
+    super._on_targeting_radius_exited(body)
+    IS_ACTIVELY_FIRING = false
+
+func _sort_mobs_by_hp(mob1: Mob, mob2: Mob) -> bool:
+    return mob1.HEALTH_BAR.HP > mob2.HEALTH_BAR.HP
+
+func _on_retargeting_timer_timeout() -> void:
+    retarget()
