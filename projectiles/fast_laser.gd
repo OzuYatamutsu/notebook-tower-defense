36ae0b0caa98307c0d1dc210f2b02108070d89f3
Sean Collins <seanc@seancotech.com>
2025-05-28 12:34:40 +0800

    Add new projectile types for new towers

---
+++ projectiles/fast_laser.gd
@@ -0,0 +1,7 @@
+class_name FastLaser
+extends Laser
+
+func _ready() -> void:
+    super._ready()
+    
+    self.DAMAGE = 2.5
---
+++ projectiles/heavy_laser.gd
@@ -0,0 +1,8 @@
+class_name HeavyLaser
+extends Laser
+
+func _ready() -> void:
+    super._ready()
+    
+    self.DAMAGE = 7.5
+    self.SPEED = 500.0
