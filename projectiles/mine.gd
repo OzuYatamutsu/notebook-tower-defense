82f847bdf1225f2cdd6cb4a292d13bb466b98e93
Sean Collins <seanc@seancotech.com>
2025-06-01 13:48:48 +0800

    Fix crash on projectile dev

--- projectiles/mine.gd
+++ projectiles/mine.gd
@@ -2,4 +2,5 @@ class_name Mine
 extends Projectile
 
 func _ready() -> void:
+    self.SPEED = 0.1  # TODO
     pass  # TODO
--- projectiles/spike.gd
+++ projectiles/spike.gd
@@ -2,4 +2,5 @@ class_name Spike
 extends Projectile
 
 func _ready() -> void:
+    self.SPEED = 0.1  # TODO
     pass  # TODO
