542c173aa1abe0e32f66a5481e7b1f2cabc97053
Sean Collins <seanc@seancotech.com>
2025-06-05 12:39:18 +0900

    Implement mine tower

---
+++ components/mine_explosion.gd
@@ -0,0 +1,27 @@
+class_name MineExplosion
+extends Area2D
+
+const EXPLOSION_GROW_SPEED: float = 2500.0
+const MAX_EXPLOSION_RADIUS: float = 500.0
+const EXPLOSION_COLOR: Color = Color(1.0, 0.4, 0.4)
+const DAMAGE: float = 45.0
+
+var radius: float = 1.0
+
+@onready var ExplosionCollider: CollisionShape2D = $ExplosionCollider
+
+func _process(delta) -> void:
+    radius += EXPLOSION_GROW_SPEED * delta
+    if radius > MAX_EXPLOSION_RADIUS:
+        _on_max_explosion_radius()
+    queue_redraw()
+
+func _on_max_explosion_radius() -> void:
+    queue_free()
+
+func _draw() -> void:
+    draw_circle(global_position, radius, EXPLOSION_COLOR)
+    ExplosionCollider.shape.radius = radius
+
+func _on_area_entered(mob: Mob) -> void:
+    mob.HEALTH_BAR.damage_by(DAMAGE)
--- projectiles/mine.gd
+++ projectiles/mine.gd
@@ -1,6 +1,25 @@
 class_name Mine
-extends Projectile
+extends Mineable
+
+const EXPLOSION_EFFECT_SCENE: PackedScene = preload("res://components/MineExplosion.tscn")
+
+@onready var ProjectileRoot: Node2D = get_tree().get_first_node_in_group(GameState.PROJECTILES_GROUP)
+
+var CurrentlyExploding: bool = false
 
 func _ready() -> void:
-    self.SPEED = 0.1  # TODO
-    pass  # TODO
+    self.DAMAGE = 75.0
+    self.EFFECTS.append(Effect.new(self.effect))
+
+func _explode() -> void:
+    if CurrentlyExploding:
+        return
+    CurrentlyExploding = true
+    
+    var explosion: MineExplosion = EXPLOSION_EFFECT_SCENE.instantiate()
+    ProjectileRoot.add_child(explosion)
+    explosion.global_position = global_position
+    queue_free()
+
+func effect(_mob: Mob) -> void:
+    call_deferred("_explode")
