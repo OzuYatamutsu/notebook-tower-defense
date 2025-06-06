37ed41f846e7847d45d8d2b4380163fb2693c638
Sean Collins <seanc@seancotech.com>
2025-06-06 16:29:44 +0900

    Implement poison and slow effects for poison spike

--- projectiles/poison_spike.gd
+++ projectiles/poison_spike.gd
@@ -1,6 +1,39 @@
 class_name PoisonSpike
-extends Projectile
+extends Mineable
+
+const POISON_DAMAGE_PER_TICK: float = 2.5
+const POISON_TIMER_TICK_SECS = 0.25
+
+var SLOW_COLOR_SHADER = load("res://components/tint_green.gdshader")
+var SLOW_COLOR_SHADER_MATERIAL = ShaderMaterial.new()
 
 func _ready() -> void:
-    self.SPEED = 0.1  # TODO
-    pass  # TODO
+    self.DAMAGE = 10.0
+    self.EFFECTS.append(Effect.new(self.poison_effect))
+    self.EFFECTS.append(Effect.new(self.slow_effect))
+    SLOW_COLOR_SHADER_MATERIAL.shader = SLOW_COLOR_SHADER
+
+func poison_effect(mob: Mob) -> void:
+    call_deferred("_poison", mob)
+
+func slow_effect(mob: Mob) -> void:
+    call_deferred("_slow", mob)
+
+func _slow(mob: Mob) -> void:
+    print(mob.name + " was slowed!")
+
+    mob.SPEED *= 0.60
+    mob.SPRITE.material = SLOW_COLOR_SHADER_MATERIAL
+
+func _poison(mob: Mob) -> void:
+    print(mob.name + " was poisoned!")
+
+    # Add a POISON TIMER to the mob
+    var poison_timer = Timer.new()
+    poison_timer.wait_time = POISON_TIMER_TICK_SECS
+
+    mob.add_child(poison_timer)
+    poison_timer.timeout.connect(
+        func(): mob.HEALTH_BAR.damage_by(POISON_TIMER_TICK_SECS)
+    )
+    poison_timer.start()
