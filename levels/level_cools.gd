9d7e7d80bc574fcfbe1641bd8208bb4b116e5d76
Sean Collins <seanc@seancotech.com>
2025-06-14 09:25:16 +0900

    Populate cools level mobs

--- levels/level_cools.gd
+++ levels/level_cools.gd
@@ -6,11 +6,23 @@ func _init() -> void:
         {"res://mobs/Basic.tscn": 5},
         {"res://mobs/Basic.tscn": 10},
         {"res://mobs/Basic.tscn": 15},
-        {"res://mobs/Basic.tscn": 15, "res://mobs/FatOne.tscn": 1},
-        {"res://mobs/BigBoss.tscn": 1},
-        {"res://mobs/Basic.tscn": 20, "res://mobs/FatOne.tscn": 10},
-        {"res://mobs/Nyoom.tscn": 10},
-        {"res://mobs/Basic.tscn": 10, "res://mobs/Nyoom.tscn": 10}
+        {"res://mobs/Basic.tscn": 20, "res://mobs/FatOne.tscn": 1},
+        {"res://mobs/Basic.tscn": 25, "res://mobs/FatOne.tscn": 2},
+        {"res://mobs/Basic.tscn": 30, "res://mobs/FatOne.tscn": 3},
+        {"res://mobs/Basic.tscn": 35, "res://mobs/FatOne.tscn": 4},
+        {"res://mobs/Basic.tscn": 40, "res://mobs/FatOne.tscn": 5},
+        {"res://mobs/Basic.tscn": 45, "res://mobs/FatOne.tscn": 6},
+        {"res://mobs/Basic.tscn": 50, "res://mobs/BigBoss.tscn": 1},
+        {"res://mobs/Basic.tscn": 55, "res://mobs/Nyoom.tscn": 5, "res://mobs/FatOne.tscn": 6},
+        {"res://mobs/Basic.tscn": 60, "res://mobs/Nyoom.tscn": 6, "res://mobs/FatOne.tscn": 7},
+        {"res://mobs/Basic.tscn": 65, "res://mobs/Nyoom.tscn": 7, "res://mobs/FatOne.tscn": 8},
+        {"res://mobs/Basic.tscn": 70, "res://mobs/Nyoom.tscn": 8, "res://mobs/FatOne.tscn": 9},
+        {"res://mobs/Basic.tscn": 75, "res://mobs/Nyoom.tscn": 9, "res://mobs/FatOne.tscn": 10},
+        {"res://mobs/Basic.tscn": 80, "res://mobs/Nyoom.tscn": 10, "res://mobs/FatOne.tscn": 11},
+        {"res://mobs/Basic.tscn": 85, "res://mobs/Nyoom.tscn": 11, "res://mobs/FatOne.tscn": 12},
+        {"res://mobs/Basic.tscn": 90, "res://mobs/Nyoom.tscn": 12, "res://mobs/FatOne.tscn": 13},
+        {"res://mobs/Basic.tscn": 95, "res://mobs/Nyoom.tscn": 13, "res://mobs/FatOne.tscn": 14},
+        {"res://mobs/Basic.tscn": 100, "res://mobs/Nyoom.tscn": 15, "res://mobs/FatOne.tscn": 15, "res://mobs/BigBoss.tscn": 1}
     ]
 
 func _ready():
