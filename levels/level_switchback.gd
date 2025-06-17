920ab0d050d0b1063437b6e3c903febcbb7c569f
Sean Lam <seanl@seancotech.com>
2025-06-17 14:40:48 +0900

    Populate switchback level with mobs

--- levels/level_switchback.gd
+++ levels/level_switchback.gd
@@ -4,12 +4,20 @@ extends Level
 func _init() -> void:
     WAVE_CONTENTS = [
         {"res://mobs/Basic.tscn": 5},
-        {"res://mobs/Basic.tscn": 10},
-        {"res://mobs/Basic.tscn": 15},
-        {"res://mobs/Basic.tscn": 15, "res://mobs/FatOne.tscn": 1},
-        {"res://mobs/Basic.tscn": 20, "res://mobs/FatOne.tscn": 10},
+        {"res://mobs/Nyoom.tscn": 5},
         {"res://mobs/Nyoom.tscn": 10},
-        {"res://mobs/Basic.tscn": 10, "res://mobs/Nyoom.tscn": 10}
+        {"res://mobs/Nyoom.tscn": 25},
+        {"res://mobs/Nyoom.tscn": 50},
+        {"res://mobs/Basic.tscn": 50},
+        {"res://mobs/Basic.tscn": 50, "res://mobs/Nyoom.tscn": 50},
+        {"res://mobs/FatOne.tscn": 50},
+        {"res://mobs/FatOne.tscn": 50, "res://mobs/Nyoom.tscn": 50},
+        {"res://mobs/BigBoss.tscn": 3},
+        {"res://mobs/Nyoom.tscn": 100},
+        {"res://mobs/Basic.tscn": 50, "res://mobs/Nyoom.tscn": 150},
+        {"res://mobs/Basic.tscn": 100, "res://mobs/Nyoom.tscn": 200},
+        {"res://mobs/Basic.tscn": 250, "res://mobs/Nyoom.tscn": 250},
+        {"res://mobs/Basic.tscn": 500, "res://mobs/Nyoom.tscn": 500, "res://mobs/FatOne.tscn": 50}
     ]
 
 func _ready():
