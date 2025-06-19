7d7da0b3cbb153e28bd1de5807db9ae1c22abc96
Sean Lam <seanl@seancotech.com>
2025-06-20 02:12:51 +0900

    Populate digestion level with mobs

--- levels/level_digestion.gd
+++ levels/level_digestion.gd
@@ -3,14 +3,26 @@ extends Level
 
 func _init() -> void:
     WAVE_CONTENTS = [
-        {"res://mobs/Basic.tscn": 5},
-        {"res://mobs/Basic.tscn": 10},
-        {"res://mobs/Basic.tscn": 15},
-        {"res://mobs/Basic.tscn": 15, "res://mobs/FatOne.tscn": 1},
-        {"res://mobs/BigBoss.tscn": 1},
-        {"res://mobs/Basic.tscn": 20, "res://mobs/FatOne.tscn": 10},
+        {"res://mobs/Nyoom.tscn": 5},
         {"res://mobs/Nyoom.tscn": 10},
-        {"res://mobs/Basic.tscn": 10, "res://mobs/Nyoom.tscn": 10}
+        {"res://mobs/Nyoom.tscn": 15},
+        {"res://mobs/Nyoom.tscn": 20},
+        {"res://mobs/Nyoom.tscn": 25},
+        {"res://mobs/Basic.tscn": 5, "res://mobs/Nyoom.tscn": 25},
+        {"res://mobs/Basic.tscn": 10, "res://mobs/Nyoom.tscn": 25},
+        {"res://mobs/Basic.tscn": 15, "res://mobs/Nyoom.tscn": 25},
+        {"res://mobs/Basic.tscn": 20, "res://mobs/Nyoom.tscn": 25},
+        {"res://mobs/Basic.tscn": 25, "res://mobs/Nyoom.tscn": 25},
+        {"res://mobs/FatOne.tscn": 5, "res://mobs/Basic.tscn": 25, "res://mobs/Nyoom.tscn": 25},
+        {"res://mobs/FatOne.tscn": 10, "res://mobs/Basic.tscn": 25, "res://mobs/Nyoom.tscn": 25},
+        {"res://mobs/FatOne.tscn": 15, "res://mobs/Basic.tscn": 25, "res://mobs/Nyoom.tscn": 25},
+        {"res://mobs/FatOne.tscn": 20, "res://mobs/Basic.tscn": 25, "res://mobs/Nyoom.tscn": 25},
+        {"res://mobs/FatOne.tscn": 25, "res://mobs/Basic.tscn": 25, "res://mobs/Nyoom.tscn": 25},
+        {"res://mobs/FatOne.tscn": 50, "res://mobs/Basic.tscn": 50, "res://mobs/Nyoom.tscn": 50},
+        {"res://mobs/FatOne.tscn": 100, "res://mobs/Basic.tscn": 100, "res://mobs/Nyoom.tscn": 100},
+        {"res://mobs/FatOne.tscn": 200, "res://mobs/Basic.tscn": 200, "res://mobs/Nyoom.tscn": 200},
+        {"res://mobs/FatOne.tscn": 400, "res://mobs/Basic.tscn": 400, "res://mobs/Nyoom.tscn": 400},
+        {"res://mobs/FatOne.tscn": 500, "res://mobs/Basic.tscn": 500, "res://mobs/Nyoom.tscn": 500},
     ]
 
 func _ready():
