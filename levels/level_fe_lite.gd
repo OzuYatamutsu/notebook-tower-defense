0fdaa16f7aa6f27ec8a1a678e58d3da91add0ade
Sean Lam <seanl@seancotech.com>
2025-06-20 01:58:55 +0900

    Populate fe lite level with mobs

--- levels/level_fe_lite.gd
+++ levels/level_fe_lite.gd
@@ -3,13 +3,26 @@ extends Level
 
 func _init() -> void:
     WAVE_CONTENTS = [
-        {"res://mobs/Basic.tscn": 5},
         {"res://mobs/Basic.tscn": 10},
-        {"res://mobs/Basic.tscn": 15},
-        {"res://mobs/Basic.tscn": 15, "res://mobs/FatOne.tscn": 1},
-        {"res://mobs/Basic.tscn": 20, "res://mobs/FatOne.tscn": 10},
-        {"res://mobs/Nyoom.tscn": 10},
-        {"res://mobs/Basic.tscn": 10, "res://mobs/Nyoom.tscn": 10}
+        {"res://mobs/Basic.tscn": 20},
+        {"res://mobs/Basic.tscn": 30},
+        {"res://mobs/Nyoom.tscn": 5},
+        {"res://mobs/Basic.tscn": 50},
+        {"res://mobs/Nyoom.tscn": 15},
+        {"res://mobs/Basic.tscn": 75},
+        {"res://mobs/FatOne.tscn": 10},
+        {"res://mobs/Basic.tscn": 50, "res://mobs/Nyoom.tscn": 25},
+        {"res://mobs/BigBoss.tscn": 1},
+        {"res://mobs/Basic.tscn": 100},
+        {"res://mobs/FatOne.tscn": 50},
+        {"res://mobs/Nyoom.tscn": 100},
+        {"res://mobs/FatOne.tscn": 100, "res://mobs/Nyoom.tscn": 100},
+        {"res://mobs/BigBoss.tscn": 3},
+        {"res://mobs/Basic.tscn": 250},
+        {"res://mobs/FatOne.tscn": 250},
+        {"res://mobs/Nyoom.tscn": 250},
+        {"res://mobs/Basic.tscn": 200, "res://mobs/FatOne.tscn": 200},
+        {"res://mobs/Basic.tscn": 200, "res://mobs/FatOne.tscn": 200, "res://mobs/Nyoom.tscn": 200},
     ]
 
 func _ready():
