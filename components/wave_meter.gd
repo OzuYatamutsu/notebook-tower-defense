242422c7dd33593dc23ffa04b052c98cd6f37749
Sean Lam <seanl@seancotech.com>
2025-07-11 21:03:32 +0800

    Merge all labels into WaveMeter

--- components/wave_meter.gd
+++ components/wave_meter.gd
@@ -4,7 +4,7 @@ extends Control
 var WaveNum: int = 0
 var MaxWaveNum: int = 0
 
-@onready var WaveMeterLabel: Label = $Label
+@onready var WavesCountLabel: Label = $TimeBar/WavesCountLabel
 @onready var WaveTimerUI: TimeBar = $WaveTimerUI
 
 func _process(_delta) -> void:
@@ -17,12 +17,12 @@ func set_max_value(max_wave_num: int) -> void:
     MaxWaveNum = max_wave_num
 
 func _set_text() -> void:
-    WaveMeterLabel.text = "{0}/{1}".format([WaveNum, MaxWaveNum])
+    WavesCountLabel.text = "{0}/{1}".format([WaveNum, MaxWaveNum])
 
 func _update_color() -> void:
     # Set the color of the label based on what percentage of
     # the current wave has elapsed.
-    WaveMeterLabel.add_theme_color_override(
+    WavesCountLabel.add_theme_color_override(
         "font_color",
         clamp(WaveTimerUI.get_timer_percent(), 0.0, 1.0)
     )
