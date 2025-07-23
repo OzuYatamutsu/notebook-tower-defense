39431fab3472ff6bbee5ed492681c71496979bcd
Sean Lam <seanl@seancotech.com>
2025-07-23 16:18:38 +0800

    Zero-pad money meter to 6 digits

--- components/money_meter.gd
+++ components/money_meter.gd
@@ -1,9 +1,17 @@
 class_name MoneyMeter
 extends Control
 
+const NUM_DIGITS = 6
+
 @onready var MoneyCountLabel: Label = $MoneyCountLabel
 
 func update_value_text() -> void:
-    MoneyCountLabel.text = "{0}".format([
+    MoneyCountLabel.text = _zero_pad("{0}".format([
         GameState.PLAYER_MONEY_REMAINING
-    ])
+    ]))
+
+func _zero_pad(num_text: String) -> String:
+    var num_digits_to_add = NUM_DIGITS - num_text.length()
+    if num_digits_to_add <= 0:
+        return num_text
+    return ('0' * num_digits_to_add) + num_text
