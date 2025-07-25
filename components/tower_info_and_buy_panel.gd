13e4b6333fc5e2ee4aee38e4fc7ec5de0ccbac30
Sean Lam <seanl@seancotech.com>
2025-07-25 12:00:47 +0200

    Implement description display in buy panel on rollover

--- components/tower_info_and_buy_panel.gd
+++ components/tower_info_and_buy_panel.gd
@@ -1,23 +1,42 @@
 class_name TowerInfoAndBuyPanel
 extends Control
 
-@export var IsOpened: bool = false
+@onready var ButtonToTowerMap = {
+    "BulletTower": preload("res://towers/BulletTower.tscn").instantiate(),
+    "SlowTower": preload("res://towers/SlowTower.tscn").instantiate(),
+    "LaserTower": preload("res://towers/LaserTower.tscn").instantiate(),
+    "SpikeTower": preload("res://towers/SpikeTower.tscn").instantiate()
+}
 
-@onready var BuyItemList: Control = $BuyItemList
-
-const TOWERS_TO_BUY_LIST: Array = [
-    "res://towers/BulletTower.tscn",
-    "res://towers/SlowTower.tscn",
-    "res://towers/LaserTower.tscn",
-    "res://towers/SpikeTower.tscn"
-]
-
-const _buyItem = preload("res://components/TowerBuyItem.tscn")
+@onready var BulletTowerButton: Button = $TowerSelectPanel/BulletTower
+@onready var SlowTowerButton: Button = $TowerSelectPanel/SlowTower
+@onready var LaserTowerButton: Button = $TowerSelectPanel/LaserTower
+@onready var SpikeTowerButton: Button = $TowerSelectPanel/SpikeTower
+@onready var DescriptionPanel: Label = $DescriptionPanel
 
 func _ready() -> void:
-    BuyItemList.remove_child(BuyItemList.get_child(0))
+    DescriptionPanel.text = ""
 
-    for tower in TOWERS_TO_BUY_LIST:
-        var _item = _buyItem.instantiate()
-        _item.setup(tower)
-        BuyItemList.add_child(_item)
+    BulletTowerButton.mouse_entered.connect(
+        _on_button_rollover.bind(BulletTowerButton)
+    )
+    BulletTowerButton.mouse_exited.connect(_on_button_exit)
+    SlowTowerButton.mouse_entered.connect(
+        _on_button_rollover.bind(SlowTowerButton)
+    )
+    SlowTowerButton.mouse_exited.connect(_on_button_exit)
+    LaserTowerButton.mouse_entered.connect(
+        _on_button_rollover.bind(LaserTowerButton)
+    )
+    LaserTowerButton.mouse_exited.connect(_on_button_exit)
+    SpikeTowerButton.mouse_entered.connect(
+        _on_button_rollover.bind(SpikeTowerButton)
+    )
+    SpikeTowerButton.mouse_exited.connect(_on_button_exit)
+
+func _on_button_rollover(button: Button) -> void:
+    var tower: Tower = ButtonToTowerMap[button.name]
+    DescriptionPanel.text = tower.Description
+
+func _on_button_exit() -> void:
+    DescriptionPanel.text = ""
