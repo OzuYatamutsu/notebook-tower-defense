24d6a5749f798afa0d290c7e6bf9cab2f9fdd025
Sean Lam <seanl@seancotech.com>
2025-07-30 23:11:44 +0200

    Tower placement shadow shows the tower you will place on click

--- components/tower_info_and_buy_panel.gd
+++ components/tower_info_and_buy_panel.gd
@@ -1,38 +1,41 @@
 class_name TowerInfoAndBuyPanel
 extends Control
 
-@onready var ButtonToTowerMap = {
-    "BulletTower": preload("res://towers/BulletTower.tscn").instantiate(),
-    "SlowTower": preload("res://towers/SlowTower.tscn").instantiate(),
-    "LaserTower": preload("res://towers/LaserTower.tscn").instantiate(),
-    "SpikeTower": preload("res://towers/SpikeTower.tscn").instantiate()
+@onready var ButtonToTowerPathMap = {
+    "BulletTower": "res://towers/BulletTower.tscn",
+    "SlowTower": "res://towers/SlowTower.tscn",
+    "LaserTower": "res://towers/LaserTower.tscn",
+    "SpikeTower": "res://towers/SpikeTower.tscn"
 }
 
+@onready var ButtonToTowerMap = {
+    "BulletTower": load(ButtonToTowerPathMap["BulletTower"]).instantiate(),
+    "SlowTower": load(ButtonToTowerPathMap["SlowTower"]).instantiate(),
+    "LaserTower": load(ButtonToTowerPathMap["LaserTower"]).instantiate(),
+    "SpikeTower": load(ButtonToTowerPathMap["SpikeTower"]).instantiate()
+}
+
+@onready var TowerSelectPanel: FlowContainer = $TowerSelectPanel
 @onready var BulletTowerButton: Button = $TowerSelectPanel/BulletTower
 @onready var SlowTowerButton: Button = $TowerSelectPanel/SlowTower
 @onready var LaserTowerButton: Button = $TowerSelectPanel/LaserTower
 @onready var SpikeTowerButton: Button = $TowerSelectPanel/SpikeTower
 @onready var DescriptionPanel: Label = $DescriptionPanel
+@onready var SpawnShadow: TowerPlacementShadow = get_tree().get_first_node_in_group(
+    GameState.TOWER_PLACEMENT_SHADOW_GROUP
+)
 
 func _ready() -> void:
     DescriptionPanel.text = ""
 
-    BulletTowerButton.mouse_entered.connect(
-        _on_button_rollover.bind(BulletTowerButton)
-    )
-    BulletTowerButton.mouse_exited.connect(_on_button_exit)
-    SlowTowerButton.mouse_entered.connect(
-        _on_button_rollover.bind(SlowTowerButton)
-    )
-    SlowTowerButton.mouse_exited.connect(_on_button_exit)
-    LaserTowerButton.mouse_entered.connect(
-        _on_button_rollover.bind(LaserTowerButton)
-    )
-    LaserTowerButton.mouse_exited.connect(_on_button_exit)
-    SpikeTowerButton.mouse_entered.connect(
-        _on_button_rollover.bind(SpikeTowerButton)
-    )
-    SpikeTowerButton.mouse_exited.connect(_on_button_exit)
+    for tower: Button in TowerSelectPanel.get_children():
+        tower.mouse_entered.connect(
+            _on_button_rollover.bind(tower)
+        )
+        tower.mouse_exited.connect(_on_button_exit)
+        tower.pressed.connect(
+            _on_button_click.bind(tower)
+        )
 
 func _on_button_rollover(button: Button) -> void:
     var tower: Tower = ButtonToTowerMap[button.name]
@@ -40,3 +43,9 @@ func _on_button_rollover(button: Button) -> void:
 
 func _on_button_exit() -> void:
     DescriptionPanel.text = ""
+
+func _on_button_click(button: Button) -> void:
+    SpawnShadow.activate(
+        ButtonToTowerPathMap[button.name],
+        ButtonToTowerMap[button.name]
+    )
